63dcf01c034cebd01fc654a9c52a5df5445c7b58
syk0c4t <100176680+syk0c4t@users.noreply.github.com>
2022-03-23 21:00:28 -0600

    cambios de cactus

---
+++ cactus.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(107,142,35);
+  fill("black")
+  rect(0,500,800,200)
+  fill("yellow")
+  rect(0,590,800,20)
+  fill(176,224,230)
+  rect(0,0,800,400)
+  fill(85,107,47)
+  rect(30,360,50,120)
+  fill(0,100,0)
+  rect(280,360,30,100)
+  fill(0,128,0)
+  rect(390,360,60,70)
+}
